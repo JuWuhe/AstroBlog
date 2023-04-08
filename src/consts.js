3206f3e986e0778ab5c1925702f08fe95c12d75d
insyent <1049966282@qq.com>
2023-04-09 01:24:48 +0800

    2023-04-09

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
 export const SITE_TITLE = `Insyent Blog`;
-export const SITE_DESCRIPTION = 'Site Description';
-export const SITE_EMAIL = 'no.sql@qq.com'
+export const SITE_DESCRIPTION = 'Description';
+export const SITE_EMAIL = 'insyent@foxmail.com'
 export const SITE_NAME = '\"此后如竟没有炬火我便是唯一的光\"';
 export const SITE_URL = "https://insyent.today";

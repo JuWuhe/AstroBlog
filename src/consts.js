1d7e3e4941e8dbd926ac84df32a21c8373b5b23f
insyent <1049966282@qq.com>
2023-04-09 01:00:58 +0800

    2023-04-09

--- src/consts.js
+++ src/consts.js
@@ -1,5 +1,5 @@
-export const SITE_TITLE = `Air Blog`;
+export const SITE_TITLE = `Insyent Blog`;
 export const SITE_DESCRIPTION = 'Site Description';
 export const SITE_EMAIL = 'no.sql@qq.com'
-export const SITE_NAME = 'astro.yufengbiji.com';
-export const SITE_URL = "https://astro.yufengbiji.com";
+export const SITE_NAME = '\"此后如竟没有炬火我便是唯一的光\"';
+export const SITE_URL = "https://insyent.today";
